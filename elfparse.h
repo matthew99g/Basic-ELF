2cce190db2a9ab79947d7702c23ed67b273c4def
Matthew T Geiger <matthewgeiger99@gmail.com>
2018-12-24 23:05:04 -0800

    Update elfparse.h

--- elfparse.h
+++ elfparse.h
@@ -16,21 +16,18 @@ void readMagicMZ(char *szPtrELF, Elf32_Ehdr *eHead) {
 // Find/Check Elf Header
 void getELFHeader(const char* elfFile, Elf32_Ehdr *eHead) {
   Elf32_Ehdr header;
+  char szElf[4];
 
   FILE* file = fopen(elfFile, "r+");
   if(file) {
     fread(&header, 1, sizeof(header), file);
 
-    if ((header.e_ident[0] == 0x7f && header.e_ident[1] == 'E' &&
-       header.e_ident[2] == 'L' && header.e_ident[3] == 'F') &&
-       header.e_type & ET_EXEC &&
+    readMagicMZ(szElf, &header);
+
+    if (header.e_ident[0] == 0x7f && strcmp(szElf, "ELF") == 0 &&
        memcmp(header.e_ident, ELFMAG, SELFMAG) == 0 ) {
 
         memcpy(eHead, &header, sizeof(header));
-    } else {
-        uint8_t uNull = 0;
-
-        memcpy(eHead, &uNull, sizeof(header));
     }
   }
 
