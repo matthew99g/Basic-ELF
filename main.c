65b74ab2eb3d38f421fe1dbb7c4a04ac20470527
Matthew T Geiger <matthewgeiger99@gmail.com>
2018-12-24 17:05:40 -0800

    Basic ELF Header Example

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <link.h>
+
+#define null 0;
+
+const char szFilePath[] = "/home/okole/Projects/C/System_Programming/Pthread_adv/pthread";
+
+int readELFHeader(const char* elfFile) {
+  int iRet = null;
+
+  // switch to Elf32_Ehdr for x86 architecture.
+  Elf32_Ehdr header;
+
+  FILE* file = fopen(elfFile, "r");
+  if(file) {
+    // read the header
+    fread(&header, 1, sizeof(header), file);
+
+    // check so its really an elf file
+    if ((header.e_ident[0] == 0x7f &&
+       header.e_ident[1] == 'E' &&
+       header.e_ident[2] == 'L' &&
+       header.e_ident[3] == 'F') &&
+       memcmp(header.e_ident, ELFMAG, SELFMAG) == 0 ) {
+
+       iRet = EXIT_SUCCESS;
+    } else {
+      fclose(file);
+
+      iRet = EXIT_FAILURE;
+    }
+
+    // finally close the file
+    fclose(file);
+  }
+
+  return iRet;
+}
+
+int main(const int argc, const char *argv[]) {
+  // Call Function/Error Handle 
+  if(readELFHeader(szFilePath)) {
+    fprintf(stderr, "Invalid ELF File...\n");
+  } else {
+    printf("Valid ELF File Found...\n");
+  }
+
+  return EXIT_SUCCESS;
+}
