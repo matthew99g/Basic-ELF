6a60f56d5a9ff14d28b78ab3b922331d35c3a901
Matthew T Geiger <matthewgeiger99@gmail.com>
2018-12-24 21:33:11 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,59 +1,28 @@
-#include <elf.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <link.h>
+#include "elfparse.h"
+#include "mem.h"
 
-const char szFilePath[] = "/home/okole/Projects/C/System_Programming/Pthread_adv/pthread";
+const char szFilePath[] =
+"/home/okole/Projects/C/System_Programming/Pthread_adv/pthread";
 
-void readMagicMZ(char *szPtrELF, Elf32_Ehdr *eHead) {
-  for(uint8_t i = 1; i < 4; i++) {
-    szPtrELF[i - 1] = eHead->e_ident[i];
-  }
+int main(const int argc, const char *argv[]) {
+  // Copy Header
+  Elf32_Ehdr *eHeader = (Elf32_Ehdr *)writeMemoryHeap(sizeof(Elf32_Ehdr), "Elf32_Ehdr");
+  getELFHeader(szFilePath, eHeader);
 
-  szPtrELF[3] = '\0';
-}
-
-void readELFHeader(const char* elfFile, Elf32_Ehdr *eHead) {
-  Elf32_Ehdr header;
-
-  FILE* file = fopen(elfFile, "r+");
-  if(file) {
-    fread(&header, 1, sizeof(header), file);
-
-    if ((header.e_ident[0] == 0x7f && header.e_ident[1] == 'E' &&
-       header.e_ident[2] == 'L' && header.e_ident[3] == 'F') &&
-       header.e_type & ET_EXEC &&
-       memcmp(header.e_ident, ELFMAG, SELFMAG) == 0 ) {
-
-      memcpy(eHead, &header, sizeof(header));
-    } else {
-      memcpy(eHead, 0x0, sizeof(header));
-    }
-  }
-
-  fclose(file);
-}
-
-int main(int argc, char *argv[]) {
-  Elf32_Ehdr *eHeader = (Elf32_Ehdr *)malloc(sizeof(Elf32_Ehdr));
-  readELFHeader(szFilePath, eHeader);
-
-  if (eHeader->e_ident[0] != 0x7f) {
+  // Double Check Stability
+  if (checkEmpty(eHeader, "Elf32_Ehdr")) {
     fprintf(stderr, "Failed to find ELF Header\n");
   }
 
-  char *szELF = (char *)malloc(sizeof(char) * 4);
+  char *szELF = (char *)writeMemoryHeap(sizeof(char) * 4, "char");
 
+  // strcpy ELF Magic Number
   readMagicMZ(szELF, eHeader);
   printf("Magic String: %s\n", szELF);
 
-  free(szELF);
+  // Free Specific Address
+  freeMemoryHeapP(szELF);
 
-  free(eHeader);
+  freeMemoryHeap();
   return 0;
 }
